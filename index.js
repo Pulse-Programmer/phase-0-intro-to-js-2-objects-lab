0daa55443245ef939081e4b30438b794262a35b1
Stephen Ochieng <materiamystic@gmail.com>
2024-03-21 21:36:23 +0300

    Adds required functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {};
+
+employee.name = "Sam";
+employee.streetAddress = "20th Avenue street";
+
+function updateEmployeeWithKeyAndValue(obj = employee, key, value) {
+  const newObj = { ...obj };
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(
+  obj = employee,
+  key,
+  value,
+) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj = employee, key) {
+  const newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj = employee, key) {
+  delete obj[key];
+  return obj;
+}
